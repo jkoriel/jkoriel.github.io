5a6328c842607b931b9f02d066d619cae319a0db
Evan Rosen <evanrosen@me.com>
2019-11-16 11:59:31 -0700

    date input polyfill

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "semantic-ui-css/semantic.min.css";
 import "./style.css";
+import "nodep-date-input-polyfill";
 
 ReactDOM.render(<App />, document.getElementById("root"));
